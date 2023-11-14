e8f51c472b17168bc31b6117b20eca231c83febf
kurves <kavssimply@gmail.com>
2023-11-14 00:31:44 -0800

    printf function

--- printf.c
+++ printf.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * _printf - print according to format
+ * @format: format argument
+ * Description: print arguments according to format
+ * Return: integer value
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
 
-    int char_count = 0;
+	va_start(args, format);
+
+	int c_count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 's':
+				c_count += printf("%s", va_arg(args, char *));
+				break;
+			case 'c':
+				c_count += printf("%c", va_arg(args, int))
+				break;
+			case 'x':
+				c_count += printf("%x", va_arg(args, unsigned int));
+				break;
+			case 'd':
+				c_count += printf("%d", va_arg(args, int));
+				break;
+			default:
+				count += printf("%%");
+				break;
+			}
+		}
+		else
+		{
+			c_count += printf("%c", *format);
+		}
+		format++;
 }
